b7571b745c5513d1fc4196c601b871907e153b3e
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2020-11-13 19:40:03 +0700

    Add files via upload

---
+++ LCD/LCD.ino
@@ -0,0 +1,14 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+void setup()
+{
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0); // กำหนดให้ เคอร์เซอร์ อยู่ตัวอักษรตำแหน่งที่0 แถวที่ 1 เตรียมพิมพ์ข้อความ
+  lcd.print("HELLO WORLD!!!"); //พิมพ์ข้อความ "LCD1602 I2c Test"
+
+}
+void loop() {
+
+}
