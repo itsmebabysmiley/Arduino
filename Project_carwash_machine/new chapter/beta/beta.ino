c9265090f5ca447430e822ee0fc3b745ac54e00c
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2021-01-17 23:24:26 +0700

    fuck up

--- Project_carwash_machine/new chapter/beta/beta.ino
+++ Project_carwash_machine/new chapter/beta/beta.ino
@@ -3,8 +3,7 @@ int sw2 = D6;
 int sw3 = D7;
 int relay1 = D3;
 int relay2 = D8;
-int sw1_state = HIGH;
-int sw2_state = HIGH;
+int coin_signal = D5;
 
 int coin = 0;
 void setup() {
@@ -12,8 +11,9 @@ void setup() {
   Serial.begin(115200);
   pinMode(sw1, INPUT);
   pinMode(sw2, INPUT);
-  //pinMode(sw3, INPUT);
-  attachInterrupt(digitalPinToInterrupt(sw3), getCoin, RISING );
+  pinMode(sw3, INPUT);
+//  attachInterrupt(digitalPinToInterrupt(sw3), getCoin, RISING );
+  pinMode(coin_signal, INPUT);
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
   digitalWrite(sw1, LOW);
@@ -22,45 +22,68 @@ void setup() {
   digitalWrite(relay1, LOW);
   digitalWrite(relay2, LOW);
 }
+boolean a = false;
 boolean c = true;
 boolean k = true;
+boolean d = false;
+boolean sw1_state = true;
+boolean sw2_state = true;
+int count = 0;
+boolean check = true;
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.println("Insert coin");
-  if(c){
-    Serial.println("Insert coin");
-    c = false;
+  L1:
+  int coin_s = digitalRead(coin_signal);
+  if(coin_s){
+    coin += 5;
+    delay(40);
+    coin_s = 0;
+    d = true;
   }
-  while(coin>0){
-    delay(50);
-    if(k){
-      Serial.printf("coin: %d\n", coin);
-      Serial.println("Choose water(1) or foam(2)");  
-      k = false;
-    }
+  if(d){
+    Serial.printf("coin: %d\n", coin);
+    Serial.println("Choose water(1) or foam(2)"); 
+    Serial.print("sw1 state=");Serial.println(sw1_state);
+    Serial.print("sw2 state=");Serial.println(sw2_state);
+    d = false;
+  }
+  if((digitalRead(sw1) || digitalRead(sw2)) && coin > 0){
+    d = true;
     if(digitalRead(sw1)){
       while(digitalRead(sw1)){
         delay(50);
       }
+      sw1_state = true;
       //water();
       doCountTime("water", sw1, relay1, 3);
       k = true;
       if(coin <= 0 ){
         c = true; 
       }
+      if(sw1_state && sw2_state){
+        goto L1;
+      }
     }//switch 1
     if(digitalRead(sw2)){
       while(digitalRead(sw2)){
         delay(50);
       }
+      sw2_state = true;
       //foam();
       doCountTime("foam", sw2, relay2, 1);
       k = true;
       if(coin <= 0 ){
         c = true; 
       }
+      if(sw1_state && sw2_state){
+        goto L1;
+      }
     }//switch2
-  }//while coin
+    if(digitalRead(sw3)){
+      setting();
+    }
+  }//if 3 condition 
 }//loop
 
 void doCountTime(String type,int sw ,int relay, int _time){
@@ -78,6 +101,11 @@ void doCountTime(String type,int sw ,int relay, int _time){
       }
       digitalWrite(relay, LOW);
       Serial.println("Paused");
+      if(type.equals("water")){
+        sw1_state = true;
+      }else{
+        sw2_state = true;
+      }
       return;
     }
     if(cTime-pTime >= 1000){
@@ -93,8 +121,15 @@ void doCountTime(String type,int sw ,int relay, int _time){
     }
   }//while coin
   digitalWrite(relay, LOW);
+  if(type.equals("water")){
+    sw1_state = true;
+  }else{
+    sw2_state = true;
+  }
 }//docounttime
 
-ICACHE_RAM_ATTR void getCoin(){
-  coin = 10;
+void setting(){
+  Serial.println("Setting");
+  delay(1000);
+  
 }
